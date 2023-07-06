5b1bec2f6a19371ce526c36c9caadba0fa4dc290
Utsav-git <utsavsalokhe@gmail.com>
2023-07-06 07:41:26 +0530

    app layout structured

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    // BEM Naming Convention eg: className = "app"
+    <div className="app">
+      <h3>Slack Clone </h3>
+      {/* Header */}
+      {/* Sidebar */}
+      {/* React Router for Chat screen */}
+    </div>
+  );
 }
 
 export default App;
